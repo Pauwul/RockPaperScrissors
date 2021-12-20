b500a6a33738bf9146ab3db8a0881df224d16fc2
Pauwul <petrila.paulcosmin@gmail.com>
2021-12-20 15:40:28 +0200

    Readable code

--- functions.js
+++ functions.js
@@ -4,12 +4,14 @@
         //functions
 
         function playerPlay(){
+            //Prompt for user 
             let playerChoice = prompt("Choose one Rock paper or scrissors ");
             return playerChoice;
 
         }
         function computerPlay(){
             const choices = ['Rock','Paper','Scrissors'];
+            //random choice
             const item = Math.floor(Math.random() * choices.length);
             return choices[item];
         }
@@ -20,31 +22,37 @@
             // Paper -> Rock
             let P = player.toUpperCase();
             let C = computer.toUpperCase();
+            //conditions for Player and Computer to Tie
             if ( P === C)
             {
                 console.log("==Tie==");
                 return 0;
             }
+            //conditions for Player to lose
             else if(( P === "ROCK" && C==="PAPER") || ( P === "PAPER" && C=== "SCRISSORS") || ( P ==="SRISSORS" && C === "PAPER"))
                 {
-                    console.log("==You lose==")
+                    console.log("==You lose==");
                     return -1;
                 }
+                //conditions for Player to win
                 else if(( C === "ROCK" && P==="PAPER") || ( C === "PAPER" && P=== "SCRISSORS") || ( C ==="SRISSORS" && P === "PAPER"))
                 {
-                    console.log("==You win==")
+                    console.log("==You win==");
                     return 1;
                 }
+                    //unintended value
+                    else console.log("==You introduced a wrong string==");
               
         }
         function game(){
-            //win counters
+            //win counters for Player and Computer
             let contorP = 0;
             let contorC = 0;
             let u = 0;
             let i = 0;
+            //loop for n=5 times
             for(i=1;i<=5;i++)
-            {   //Round header
+            {   //Round counter
                 console.log("Round: ", i);
                 //computer decision
                 let pc = computerPlay();
@@ -53,13 +61,18 @@
                 
                 console.log("Computer's choice:",cc);
                 console.log("Your choice:",pc);
+                //judge decides which one should win and returns -1, 0, 1 accordingly
+                Winner = judge(cc, pc);
+                console.log("Winner = ", Winner);
 
-                u =judge(cc, pc);
-                console.log("u = ", u);
-                if( u === -1)
-                   { contorC = contorC + 1; }
-                if( u === 1)
-                   { contorP++;}
+                if( Winner === -1)
+                { 
+                    contorC = contorC + 1; 
+                }
+                if( Winner === 1)
+                { 
+                       contorP++;
+                }
                 
 
             }
@@ -70,5 +83,5 @@
                 else 
                 console.log("\n\n>>>Tie<<<<")
         }
-
+        //Function call
         game()
